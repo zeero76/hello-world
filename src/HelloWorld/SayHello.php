be436c6a8e38930c3fa3ff11f677473dbc9afeaa
Marek Smatlak <marek.smatlak@smartweb.sk>
2021-06-29 10:10:20 +0200

    first commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace zeero76\HelloWorld;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'zeero76/hello-world',
+  ),
+  'versions' => 
+  array (
+    'zeero76/hello-world' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
